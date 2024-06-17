61f988cecabf27bfff6ffaf1360a5be2f25f388c
Ralvesbraga <ralvesbraga40@gmail.com>
2024-06-16 19:40:34 -0500

    resoluçao de problemas usando estrutura de dados

--- dataStructure/src/resolucao_problemas/MaiorValorABB.java
+++ dataStructure/src/resolucao_problemas/MaiorValorABB.java
@@ -1,2 +1,42 @@
-package resolucao_problemas;public class MaiorValorABB {
+package resolucao_problemas;
+
+import data_structure_ABB.Abb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaiorValorABB {
+
+    public static List<Integer> largestValues(Abb tree){
+        var list = new ArrayList<Integer>();
+        solveTree(tree.root, 0, list);
+        return list;
+    }
+
+    private static void solveTree(Abb.Node node, int level, ArrayList<Integer> list) {
+        if(node == null) return;
+
+        if (level == list.size()){
+            list.add(node.value);
+        }else{
+            list.set(level, Math.max(list.get(level), node.value));
+        }
+        solveTree(node.left, level+1,list);
+        solveTree(node.right,level+1,list);
+    }
+
+    public static void main(String[] args) {
+        Abb tree = new Abb();
+        tree.insert(100);
+
+        tree.insert(10);
+        tree.insert(5);
+
+        tree.insert(6);
+        tree.insert(1);
+        tree.insert(7);
+        tree.insert(5);
+
+        System.out.println(largestValues(tree));
+    }
 }
--- dataStructure/src/resolucao_problemas/ProblemaColchete.java
+++ dataStructure/src/resolucao_problemas/ProblemaColchete.java
@@ -1,2 +1,32 @@
-package resolucao_problemas;public class ProblemaColchete {
+package resolucao_problemas;
+
+import java.util.Set;
+import java.util.Stack;
+
+public class ProblemaColchete {
+
+
+    public static void main(String[] args) {
+        System.out.println(solve("2*(3+4+5*[2+3)])"));
+        System.out.println(solve("([{}])"));
+    }
+
+    public static boolean solve(String expression){
+        Set<Character> openSymbols = Set.of('(', '[', '{');
+        Set<Character> closeSymbols = Set.of(')', ']', '}');
+        Stack<Character> stack = new Stack<>();
+
+        for(char c: expression.toCharArray()){
+            if(openSymbols.contains(c)) stack.push(c);
+            if(closeSymbols.contains(c)) {
+                if (stack.isEmpty()) return false;
+                var open = stack.pop();
+                if(open == '(' && c != ')') return false;
+                if(open == '[' && c != ']') return false;
+                if(open == '{' && c != '}') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
 }
